67f37ecd8d374aca0f1afef4452a00e52597d85f
rolancia <95jjangjun@gmail.com>
2021-10-27 21:29:05 +0900

    feat: fetch products when app starts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,26 @@
+import { Component, Composable } from './core/component';
+import { ProductListPage } from './pages/product-list';
+import { HttpClient, HttpClientImpl } from './service/http-client';
+import { IMAGE_DOMAIN_ENDPOINT } from './config/config';
+import { Product } from './components/product';
+import './styles.css';
+
 class App {
-  constructor(private readonly root: HTMLDivElement) {}
+  productListPage: Component & Composable;
+  constructor(private readonly root: HTMLDivElement, private readonly httpClient: HttpClient) {
+    this.productListPage = new ProductListPage();
+    this.productListPage.attachTo(this.root);
+
+    this.httpClient.getProducts().then((products) => {
+      products.forEach((product) => {
+        const { id, name, imageUrl, price } = product;
+        this.productListPage.addChild(new Product(id, name, imageUrl, price));
+      });
+    });
+  }
 }
 
-new App(document.querySelector('#root')! as HTMLDivElement);
+new App(
+  document.querySelector('#root')! as HTMLDivElement,
+  new HttpClientImpl(IMAGE_DOMAIN_ENDPOINT)
+);
